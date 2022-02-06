8d54b9de649a9ca6637636e4d5929d32ccf7545e
Qsymond <logvinov.evgeniy@ya.ru>
2022-02-06 03:19:19 +0300

    completed

--- src/main/java/edu/school21/restful/models/roles/ERole.java
+++
@@ -1,7 +0,0 @@
-package edu.school21.restful.models.roles;
-
-public enum ERole {
-    ROLE_ADMIN,
-    ROLE_TEACHER,
-    ROLE_STUDENT
-}
--- src/main/java/edu/school21/restful/request/UserRequest.java
+++
@@ -1,8 +0,0 @@
-package edu.school21.restful.request;
-
-import lombok.Data;
-
-@Data
-public class UserRequest {
-    private String userId;
-}
